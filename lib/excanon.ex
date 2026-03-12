f356fb8a438490205f7b7406c39b3a136678ca21
jlovera <juanma>
2026-03-11 20:50:16 -0500

    feat: Initial commit

---
+++ lib/excanon.ex
@@ -0,0 +1,18 @@
+defmodule Excanon do
+  @moduledoc """
+  Documentation for `Excanon`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Excanon.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/excanon_test.exs
@@ -0,0 +1,8 @@
+defmodule ExcanonTest do
+  use ExUnit.Case
+  doctest Excanon
+
+  test "greets the world" do
+    assert Excanon.hello() == :world
+  end
+end
