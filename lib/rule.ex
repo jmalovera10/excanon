7573d7115b572ad2b79aac70b6a7752554db503a
jmalovera10 <8958924+jmalovera10@users.noreply.github.com>
2026-08-03 20:24:48 -0500

    feat: Add validation for required fields in Rule struct

--- lib/rule.ex
+++ lib/rule.ex
@@ -13,14 +13,26 @@ defmodule Rule do
           requires: list(String.t())
         }
 
+  def new!(params) when not is_map_key(params, "name"),
+    do: raise(ArgumentError, "Name field is required")
+
   def new!(%{"name" => name}) when not is_binary(name),
     do: raise(ArgumentError, "Name field is required")
+
+  def new!(params) when not is_map_key(params, "description"),
+    do: raise(ArgumentError, "Description field is required")
 
   def new!(%{"description" => description}) when not is_binary(description),
     do: raise(ArgumentError, "Description field is required")
 
+  def new!(params) when not is_map_key(params, "conditions"),
+    do: raise(ArgumentError, "Conditions field is required")
+
   def new!(%{"conditions" => conditions}) when not is_map(conditions),
     do: raise(ArgumentError, "Conditions field is required")
+
+  def new!(params) when not is_map_key(params, "actions"),
+    do: raise(ArgumentError, "Actions field is required")
 
   def new!(%{"actions" => actions}) when not is_list(actions),
     do: raise(ArgumentError, "Actions field is required")
