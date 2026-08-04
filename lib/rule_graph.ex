4052dbe49b52c5eb9c06c4f8d12e52a924ac4026
jmalovera10 <8958924+jmalovera10@users.noreply.github.com>
2026-08-03 22:39:36 -0500

    refactor: Simplify rule validation by consolidating unknown reference checks and removing cycle detection

--- lib/rule_graph.ex
+++ lib/rule_graph.ex
@@ -4,17 +4,16 @@ defmodule RuleGraph do
   @spec build([Rule.t()]) :: {:ok, [Rule.t()]} | {:error, String.t()}
   def build(rules) when is_list(rules) do
     with :ok <- check_duplicate_names(rules),
-         :ok <- check_unknown_references(rules),
-         :ok <- check_cycles(rules) do
-      {:ok, topological_sort(rules)}
+         :ok <- check_unknown_references(rules) do
+      topological_sort(rules)
     end
   end
 
   defp check_duplicate_names(rules) do
     names = Enum.map(rules, & &1.name)
-    duplicated = names -- Enum.uniq(names)
+    frequencies = Enum.frequencies(names)
 
-    case Enum.filter(Enum.uniq(names), &(&1 in duplicated)) do
+    case Enum.filter(Enum.uniq(names), &(Map.fetch!(frequencies, &1) > 1)) do
       [] -> :ok
       dups -> {:error, "Duplicate rule names: #{Enum.join(dups, ", ")}"}
     end
@@ -23,81 +22,57 @@ defmodule RuleGraph do
   defp check_unknown_references(rules) do
     known_names = MapSet.new(rules, & &1.name)
 
-    with :ok <- check_after_references(rules, known_names) do
-      check_requires_references(rules, known_names)
+    case find_unknown_reference(rules, known_names) do
+      nil ->
+        :ok
+
+      {:after, rule_name, ref} ->
+        {:error, "Rule '#{rule_name}' references unknown rule '#{ref}' in after"}
+
+      {:requires, rule_name, ref} ->
+        {:error, "Rule '#{rule_name}' requires unknown rule '#{ref}'"}
     end
   end
 
-  defp check_after_references(rules, known_names) do
-    case find_unknown_reference(rules, known_names, & &1.after) do
-      nil -> :ok
-      {rule_name, ref} -> {:error, "Rule '#{rule_name}' references unknown rule '#{ref}' in after"}
-    end
-  end
-
-  defp check_requires_references(rules, known_names) do
-    case find_unknown_reference(rules, known_names, & &1.requires) do
-      nil -> :ok
-      {rule_name, ref} -> {:error, "Rule '#{rule_name}' requires unknown rule '#{ref}'"}
-    end
-  end
-
-  defp find_unknown_reference(rules, known_names, get_refs) do
+  defp find_unknown_reference(rules, known_names) do
     Enum.find_value(rules, fn rule ->
-      find_unknown_ref_in(rule.name, get_refs.(rule), known_names)
+      find_unknown_ref_in(rule.name, :after, rule.after, known_names) ||
+        find_unknown_ref_in(rule.name, :requires, rule.requires, known_names)
     end)
   end
 
-  defp find_unknown_ref_in(rule_name, refs, known_names) do
+  defp find_unknown_ref_in(rule_name, field, refs, known_names) do
     Enum.find_value(refs, fn ref ->
-      if not MapSet.member?(known_names, ref), do: {rule_name, ref}
+      if not MapSet.member?(known_names, ref), do: {field, rule_name, ref}
     end)
-  end
-
-  defp check_cycles(rules) do
-    graph = :digraph.new()
-
-    try do
-      Enum.each(rules, fn rule -> :digraph.add_vertex(graph, rule.name) end)
-
-      Enum.each(rules, fn rule ->
-        Enum.each(rule.after ++ rule.requires, fn ref ->
-          :digraph.add_edge(graph, ref, rule.name)
-        end)
-      end)
-
-      cyclic_names =
-        graph
-        |> :digraph_utils.strong_components()
-        |> Enum.filter(&(length(&1) > 1))
-        |> List.flatten()
-        |> Kernel.++(:digraph_utils.loop_vertices(graph))
-        |> Enum.uniq()
-
-      case cyclic_names do
-        [] ->
-          :ok
-
-        names ->
-          ordered_names =
-            rules
-            |> Enum.map(& &1.name)
-            |> Enum.filter(&(&1 in names))
-
-          {:error, "Cycle detected among rules [#{Enum.join(ordered_names, ", ")}]"}
-      end
-    after
-      :digraph.delete(graph)
-    end
   end
 
   defp topological_sort(rules) do
-    index_by_name = rules |> Enum.with_index() |> Map.new(fn {rule, idx} -> {rule.name, idx} end)
-    rule_by_name = Map.new(rules, &{&1.name, &1})
+    meta_by_name =
+      rules |> Enum.with_index() |> Map.new(fn {rule, idx} -> {rule.name, {idx, rule}} end)
+
     adjacency = build_adjacency(rules)
     in_degree = build_in_degree(rules)
+    ready = ready_set(in_degree, meta_by_name)
 
-    kahn_sort(adjacency, in_degree, index_by_name, rule_by_name, [])
+    kahn_sort(adjacency, in_degree, meta_by_name, ready, [])
+  end
+
+  defp ready_set(in_degree, meta_by_name) do
+    in_degree
+    |> Enum.filter(fn {_name, degree} -> degree == 0 end)
+    |> Enum.map(fn {name, _degree} -> {index_of(meta_by_name, name), name} end)
+    |> :gb_sets.from_list()
+  end
+
+  defp index_of(meta_by_name, name) do
+    {index, _rule} = Map.fetch!(meta_by_name, name)
+    index
+  end
+
+  defp rule_of(meta_by_name, name) do
+    {_index, rule} = Map.fetch!(meta_by_name, name)
+    rule
   end
 
   defp build_adjacency(rules) do
@@ -114,26 +89,49 @@ defmodule RuleGraph do
     Map.new(rules, &{&1.name, length(&1.after) + length(&1.requires)})
   end
 
-  defp kahn_sort(_adjacency, in_degree, _index_by_name, _rule_by_name, acc)
-       when map_size(in_degree) == 0 do
-    Enum.reverse(acc)
+  defp kahn_sort(adjacency, in_degree, meta_by_name, ready, acc) do
+    if :gb_sets.is_empty(ready) do
+      if map_size(in_degree) == 0 do
+        {:ok, Enum.reverse(acc)}
+      else
+        cycle_error(in_degree, meta_by_name)
+      end
+    else
+      {{_index, next_name}, remaining_ready} = :gb_sets.take_smallest(ready)
+      in_degree = Map.delete(in_degree, next_name)
+
+      {in_degree, ready} = update_in_degree(adjacency, in_degree, remaining_ready, meta_by_name, next_name)
+
+      kahn_sort(adjacency, in_degree, meta_by_name, ready, [
+        rule_of(meta_by_name, next_name) | acc
+      ])
+    end
   end
 
-  defp kahn_sort(adjacency, in_degree, index_by_name, rule_by_name, acc) do
-    {next_name, _degree} =
+  defp update_in_degree(adjacency, in_degree, remaining_ready, meta_by_name, next_name) do
+    adjacency
+    |> Map.fetch!(next_name)
+    |> Enum.reduce({in_degree, remaining_ready}, fn dependent, {degree_acc, ready_acc} ->
+      new_degree = Map.fetch!(degree_acc, dependent) - 1
+      degree_acc = Map.put(degree_acc, dependent, new_degree)
+
+      ready_acc =
+        if new_degree == 0 do
+          :gb_sets.add({index_of(meta_by_name, dependent), dependent}, ready_acc)
+        else
+          ready_acc
+        end
+
+      {degree_acc, ready_acc}
+    end)
+  end
+
+  defp cycle_error(in_degree, meta_by_name) do
+    names =
       in_degree
-      |> Enum.filter(fn {_name, degree} -> degree == 0 end)
-      |> Enum.min_by(fn {name, _degree} -> Map.fetch!(index_by_name, name) end)
+      |> Map.keys()
+      |> Enum.sort_by(&index_of(meta_by_name, &1))
 
-    updated_in_degree =
-      adjacency
-      |> Map.fetch!(next_name)
-      |> Enum.reduce(Map.delete(in_degree, next_name), fn dependent, acc2 ->
-        Map.update!(acc2, dependent, &(&1 - 1))
-      end)
-
-    kahn_sort(adjacency, updated_in_degree, index_by_name, rule_by_name, [
-      Map.fetch!(rule_by_name, next_name) | acc
-    ])
+    {:error, "Cycle detected among rules [#{Enum.join(names, ", ")}]"}
   end
 end
